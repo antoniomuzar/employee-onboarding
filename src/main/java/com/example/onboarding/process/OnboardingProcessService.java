e29fc8c924f10529a1a657f64e423a12e251e0ad
Antonio Muzar <antonio.muzar@email.com>
2026-02-01 20:16:42 +0100

    Add automatic tasks and fix status badge

--- src/main/java/com/example/onboarding/process/OnboardingProcessService.java
+++ src/main/java/com/example/onboarding/process/OnboardingProcessService.java
@@ -2,20 +2,32 @@ package com.example.onboarding.process;
 
 import com.example.onboarding.employee.Employee;
 import com.example.onboarding.employee.EmployeeService;
+import com.example.onboarding.task.OnboardingTaskService;
+import com.example.onboarding.task.OnboardingTaskType;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class OnboardingProcessService {
 
+    private final List<PredefinedTask> PRE_EMPLOYMENT_TASKS = List.of(
+            new PredefinedTask("Sign contract", OnboardingTaskType.HR),
+            new PredefinedTask("Provide personal documents", OnboardingTaskType.HR),
+            new PredefinedTask("Prepare laptop", OnboardingTaskType.IT)
+    );
+
     private final EmployeeService employeeService;
     private final OnboardingStatusTransitionService onboardingStatusTransitionService;
+    private final OnboardingTaskService onboardingTaskService;
 
 
-    public OnboardingProcessService(EmployeeService employeeService, OnboardingStatusTransitionService onboardingStatusTransitionService) {
+    public OnboardingProcessService(EmployeeService employeeService, OnboardingStatusTransitionService onboardingStatusTransitionService, OnboardingTaskService onboardingTaskService) {
         this.employeeService = employeeService;
         this.onboardingStatusTransitionService = onboardingStatusTransitionService;
+        this.onboardingTaskService = onboardingTaskService;
     }
 
     public void changeStatus(Long employeeId, OnboardingStatus targetStatus) {
@@ -51,6 +63,20 @@ public class OnboardingProcessService {
 
         employee.setStatus(nextStatus);
         employeeService.save(employee);
+
+        if(nextStatus == OnboardingStatus.PRE_EMPLOYMENT){
+            createPreEmploymentTasks(employee);
+        }
+    }
+
+    public void createPreEmploymentTasks(Employee employee){
+    PRE_EMPLOYMENT_TASKS.forEach(task->
+            onboardingTaskService.createTaskForEmployee(
+                    employee,
+                    task.name(),
+                    task.type()
+            )
+    );
     }
 }
 
---
+++ src/main/java/com/example/onboarding/process/PredefinedTask.java
@@ -0,0 +1,7 @@
+package com.example.onboarding.process;
+
+import com.example.onboarding.task.OnboardingTaskType;
+
+public record PredefinedTask(String name, OnboardingTaskType type) {
+
+}
